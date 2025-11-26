4b9971ef0cc112cd07035fb2f433a4dac83259cc
mohit-18-tech <MohitRawat.23037@stu.upes.ac.in>
2025-11-27 00:11:17 +0530

    Refactor character case conversion in string

--- Day70 Q120.c
+++ Day70 Q120.c
@@ -9,18 +9,18 @@ int main() {
     char str[200];
     int i;
 
-    // Read full line including spaces
+    
     fgets(str, sizeof(str), stdin);
 
     for (i = 0; str[i] != '\0'; i++) {
-        // If it's the first character or the previous character is a space
+        
         if (i == 0 || str[i - 1] == ' ') {
-            // Convert to uppercase if it's a lowercase letter
+           
             if (str[i] >= 'a' && str[i] <= 'z')
                 str[i] = str[i] - 32;
         } 
         else {
-            // Convert to lowercase if it's an uppercase letter (to keep consistent)
+           
             if (str[i] >= 'A' && str[i] <= 'Z')
                 str[i] = str[i] + 32;
         }
