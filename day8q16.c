62760d71412cda4243e0f248bc501c87c4c8a60a
mohit-18-tech <MohitRawat.23037@stu.upes.ac.in>
2025-11-27 15:08:09 +0530

    Implement program to find the largest of three numbers

---
+++ day8q16.c
@@ -0,0 +1,20 @@
+//  Q16: Write a program to find the largest of three numbers.
+ 
+ // PROGRAM:
+
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter three numbers: ");
+    scanf("%d,%d,%d",&a,&b,&c);
+    if(a>=b && a>=c){
+        printf("%d is the largest number.",a);
+    }
+    else if(b>=a && b>=c){
+        printf("%d is the largest number.",b);
+    }
+    else{
+        printf("%d is the largest number.",c);
+    }
+    return 0;
+}
