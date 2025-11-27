c9a6b8e77bcc7467e1e92e1d2fa62d24585ea6c0
mohit-18-tech <MohitRawat.23037@stu.upes.ac.in>
2025-11-27 14:55:06 +0530

    Add anagram checking program in C
    Implement a C program to check if two strings are anagrams.

---
+++ day47q93.c
@@ -0,0 +1,42 @@
+// Q93: Write a C program to check whether two strings are anagrams of each other or not.
+
+ 
+
+#include <stdio.h>
+
+int main() {
+    char str1[100], str2[100];
+    int freq[26] = {0};
+    int i;
+
+    
+    scanf("%s", str1);
+    scanf("%s", str2);
+
+    
+    for (i = 0; str1[i] != '\0'; i++) {
+        if (str1[i] >= 'a' && str1[i] <= 'z')
+            freq[str1[i] - 'a']++;
+        else if (str1[i] >= 'A' && str1[i] <= 'Z')
+            freq[str1[i] - 'A']++;
+    }
+
+   
+    for (i = 0; str2[i] != '\0'; i++) {
+        if (str2[i] >= 'a' && str2[i] <= 'z')
+            freq[str2[i] - 'a']--;
+        else if (str2[i] >= 'A' && str2[i] <= 'Z')
+            freq[str2[i] - 'A']--;
+    }
+
+    
+    for (i = 0; i < 26; i++) {
+        if (freq[i] != 0) {
+            printf("Not anagrams");
+            return 0;
+        }
+    }
+
+    printf("Anagrams");
+    return 0;
+}
