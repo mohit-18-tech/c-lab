d83205d3f65a8b298925bbb153df26cd8bd3572d
mohit-18-tech <MohitRawat.23037@stu.upes.ac.in>
2025-11-27 15:11:06 +0530

    Update day7q13.c

---
+++ day7q13.c
@@ -0,0 +1,16 @@
+ 
+ // PROGRAM:
+
+#include<stdio.h>
+int main(){
+    int yr;
+    printf("Enter a year: ");
+    scanf("%d",&yr);
+    if((yr%4==0 && yr%100!=0) || (yr%400==0)){
+        printf("%d is a leap year.\n",yr);
+    }
+    else{
+        printf("%d is not a leap year.\n",yr);
+    }
+  return 0;
+}
