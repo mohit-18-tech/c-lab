4d28964e4c03764f926107e5b7f1f22de11bc7a5
mohit-18-tech <MohitRawat.23037@stu.upes.ac.in>
2025-11-27 15:11:40 +0530

    Update day7q14.c

---
+++ day7q14.c
@@ -0,0 +1,17 @@
+// Q14: Write a program to check whether a character is a vowel or consonant.
+ 
+ // PROGRAM:
+
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c",&ch);
+    if((ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') || (ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')){
+        printf("%c is a vowel.\n",ch);
+    }
+    else{
+        printf("%c is a consonant.\n",ch);
+    }
+  return 0;
+}
