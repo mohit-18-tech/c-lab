d351c60ce6e191d5b141c9a01ea7b2e7761d1a68
mohit-18-tech <MohitRawat.23037@stu.upes.ac.in>
2025-11-27 14:25:40 +0530

    Update day21q41.c

--- day21q41.c
+++ day21q41.c
@@ -8,22 +8,23 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Find the last digit
+   
     lastDigit = num % 10;
 
-    // Find the number of digits
+   
     digits = (int)log10(num);
 
-    // Find the first digit
+    
     firstDigit = (int)(num / pow(10, digits));
 
-    // Remove the first and last digits from the number
-    num = num % (int)pow(10, digits); // Remove first digit
-    num = num / 10; // Remove last digit
+    
+    num = num % (int)pow(10, digits); 
+    num = num / 10; 
 
-    // Reconstruct the number with swapped digits
+    
     swappedNum = lastDigit * (int)pow(10, digits) + num * 10 + firstDigit;
 
     printf("Swapped number: %d\n", swappedNum);
     return 0;
 }
+
