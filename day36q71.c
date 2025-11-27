4856fc624a34076e813aa4f8a410080870537382
mohit-18-tech <MohitRawat.23037@stu.upes.ac.in>
2025-11-27 14:37:30 +0530

    Implement matrix input and display in C
    This program reads a matrix of size m x n from user input and displays it.

---
+++ day36q71.c
@@ -0,0 +1,31 @@
+// Q71: Write a C program to read and display a matrix of size m x n.
+
+ // PROGRAM:
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+
+    printf("Enter elements of the matrix:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("The matrix is:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
