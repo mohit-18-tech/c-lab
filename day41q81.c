79bd60216ebb459545debf20548fbc1f4c2cd0d5
mohit-18-tech <MohitRawat.23037@stu.upes.ac.in>
2025-11-27 14:46:25 +0530

    Update day41q81.c

---
+++ day41q81.c
@@ -0,0 +1,23 @@
+// Q81: Write a C program to count the number of characters in a string without using built-in functions.
+
+
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+
+    
+    gets(str); 
+
+   
+    while (str[count] != '\0') {
+        count++;
+    }
+
+    
+    printf("%d", count);
+
+    return 0;
+}
