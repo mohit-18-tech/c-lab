62fedf54b0b59ce465f18511df903ec484c04386
mohit-18-tech <MohitRawat.23037@stu.upes.ac.in>
2025-11-27 14:29:03 +0530

    Implement sum calculation for array elements in C
    This program calculates the sum of all elements in an array input by the user.

---
+++ day29q57.c
@@ -0,0 +1,28 @@
+// Q57: Write a program in C to calculate the sum of all elements in an array.
+
+
+
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    
+    scanf("%d", &n);
+
+    int arr[n];
+
+   
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+   
+    for (i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
